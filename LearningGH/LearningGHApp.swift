7d66d24e919f66448d4ae063eb02ff9e60e14783
Armaan Costa <armaancostabusinesss@gmail.com>
2022-01-24 14:39:59 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by X on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
